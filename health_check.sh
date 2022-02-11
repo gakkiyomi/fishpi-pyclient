5933b6bab41649399941de36764bd8f835c61b28
fangcong <fangcong@sky-cloud.net>
2022-02-11 22:29:51 +0800

    feat: 健康检查脚本

---
+++ health_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true
+do
+    count=`ps -ef | grep core.py | grep -v "grep" | wc -l`
+    if [[ $count == 0 ]];then
+        echo "自动重启中"
+        sh pwl_start
+        echo "脚本重启成功"
+    else
+        echo "脚本状态正常"
+    fi
+    sleep 5
+done
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nohup python3 -u core.py >> eyre7.log 2>&1 &
